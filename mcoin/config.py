a3f2b414f98ea58351408460c9bd719e1e5e3fbf
Jackie Lee <jackylee0424@gmail.com>
2014-02-15 16:29:28 -0800

    edit test node address and port

--- mcoin/config.py
+++ mcoin/config.py
@@ -1,10 +1,10 @@
 import landerdb
 
 relay = 1
-brokers = [{"ip":"192.168.0.1", "port":6565}, {"ip":"127.0.0.1", "port":6565}]
+brokers = [{"ip":"192.168.1.121", "port":1217}, {"ip":"192.168.1.122", "port":1217}]
 version = "0.0.1"
 host = "0.0.0.0"
-port = 6565
+port = 1217
 nodes = landerdb.Connect("nodes.db")
 wallet = landerdb.Connect("wallet.db")
 db = landerdb.Connect("db.db")
